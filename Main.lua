11711e54a948b286a6f8be35dc902cc549351d0b
Trix <80095537+TrixAde@users.noreply.github.com>
2022-04-22 11:14:48 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 8528736393 then --Beatland
       loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Beatland-Script.lua"))()
- end
+end
+if game.PlaceId = 5490351219 then --Clicker Madness
+      loadstring(game:HttpGet('https://github.com/TrixAde/Proxima-Hub/blob/main/ClickerMadness.lua'))()
+end
