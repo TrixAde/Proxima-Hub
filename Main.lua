e030735d98f72815d246ab944166ef63fbdc39ea
Trix <80095537+TrixAde@users.noreply.github.com>
2022-04-22 11:50:33 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,3 +4,6 @@ end
 if game.PlaceId == 5490351219 then --Clicker Madness
       loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/ClickerMadness.lua'))()
 end
+if game.PlaceId == 7335851129 then --Roblox Pro League
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Roblox-Pro-League.lua"))()
+end
