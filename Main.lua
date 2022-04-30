72ec6b404f793532f968b1297a9ce434d75c0584
Trix <80095537+TrixAde@users.noreply.github.com>
2022-04-30 14:13:56 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,12 +1,15 @@
-if game.PlaceId == 8528736393 then --Beatland
+if game.PlaceId == 8528736393 then -- Beatland
       loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Beatland-Script.lua"))()
 end
-if game.PlaceId == 318978013 then --Kick Off
+if game.PlaceId == 318978013 then -- Kick Off
       loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/KickOff.lua'))()
 end
-if game.PlaceId == 5490351219 then --Clicker Madness
+if game.PlaceId == 5490351219 then -- Clicker Madness
       loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/ClickerMadness.lua'))()
 end
-if game.PlaceId == 7335851129 then --Roblox Pro League
+if game.PlaceId == 7335851129 then -- Roblox Pro League
       loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Roblox-Pro-League.lua"))()
+end
+if game.PlaceId == 9230434873 then -- Logitech Song Breaker Awards 
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Logitech-Song-Breaker-Awards.lua"))()
 end
