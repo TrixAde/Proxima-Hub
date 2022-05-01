be2bb3c346090fa7616d731a2e6a9a0d44b63b91
Trix <80095537+TrixAde@users.noreply.github.com>
2022-05-02 00:48:29 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,18 +1,28 @@
-if game.PlaceId == 8528736393 then -- Beatland
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Beatland-Script.lua"))()
-end
+if game.PlaceId == 1962086868 then -- Tower Of Hell
+      loadstring(game:HttpGet(('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Tower_Of_Hell.lua')))()
+end     
+
 if game.PlaceId == 318978013 then -- Kick Off
       loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/KickOff.lua'))()
 end
-if game.PlaceId == 5490351219 then -- Clicker Madness
-      loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/ClickerMadness.lua'))()
-end
-if game.PlaceId == 7335851129 then -- Roblox Pro League
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Roblox-Pro-League.lua"))()
-end
+
 if game.PlaceId == 9230434873 then -- Logitech Song Breaker Awards 
       loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Logitech-Song-Breaker-Awards.lua"))()
 end
+
 if game.PlaceId == 8523408215 then -- Alo Sanctuary
       loadstring(game:HttpGet(('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Alo-Sanctuary.lua')))()
 end
+
+if game.PlaceId == 8528736393 then -- Beatland
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Beatland-Script.lua"))()
+end
+
+if game.PlaceId == 5490351219 then -- Clicker Madness
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/ClickerMadness.lua'))()
+end
+
+if game.PlaceId == 7335851129 then -- Roblox Pro League
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Roblox-Pro-League.lua"))()
+end
+
